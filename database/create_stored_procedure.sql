37701254d88f93c789d45c5d2e6d9689f3d19828
LamTuan0101 <nguyenngoclamtuan2000@gmail.com>
2026-03-30 23:16:36 +0700

    Add stored procedures for account creation and update

--- database/create_stored_procedure.sql
+++ database/create_stored_procedure.sql
@@ -14,3 +14,38 @@ BEGIN
     INSERT INTO Notifications (Notification_ID, User_ID, Message)
     VALUES (@Notification_ID, @User_ID, @Message);
 END
+
+-- 13: Create account
+CREATE PROCEDURE CreateAccount (
+	@NewUserID VARCHAR(50),
+	@NewName VARCHAR(255),
+	@NewEmail VARCHAR(255),
+	@NewPhone VARCHAR(255),
+	@NewPassword VARCHAR(255)
+)
+AS
+BEGIN	
+
+    INSERT INTO [dbo].[Users] ([User_ID], [Name], [Email], [Phone], [Password])
+     VALUES(@NewUserID, @NewName, @NewEmail, @NewPhone, @NewPassword)
+END
+GO
+
+-- 14: Update information of user
+CREATE PROCEDURE UpdateInformation (
+	@UserID VARCHAR(50),
+	@NewName VARCHAR(255),
+	@NewEmail VARCHAR(255),
+	@NewPhone VARCHAR(255),
+	@NewPassword VARCHAR(255)
+)
+AS
+BEGIN	
+	UPDATE Users
+	SET Name = @NewName,
+		Email = @NewEmail,
+		Phone = @NewPhone,
+		Password = @NewPassword
+	WHERE User_ID = @UserID
+END
+GO
