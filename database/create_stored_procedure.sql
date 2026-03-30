209f01e283fed78b553a9c0ca49bfe55d5989bdb
HuyTuan <tuanhuy2342006@gmail.com>
2026-03-30 22:17:25 +0700

    gửi thông báo cho người dùng

--- database/create_stored_procedure.sql
+++ database/create_stored_procedure.sql
@@ -3,3 +3,14 @@ use hotel_management;
 -- xóa các Stored Procedure nếu đã tồn tại
 
 -- viet code tạo Stored Procedure ở đây
+-- chức năng 8
+-- tạo produce để gửi thông báo cho người dùng
+CREATE PROCEDURE SP_gui_Notification
+    @Notification_ID VARCHAR(50),
+    @User_ID VARCHAR(50),
+    @Message NVARCHAR(1000)
+AS
+BEGIN
+    INSERT INTO Notifications (Notification_ID, User_ID, Message)
+    VALUES (@Notification_ID, @User_ID, @Message);
+END
--- database/create_trigger.sql
+++ database/create_trigger.sql
@@ -3,3 +3,18 @@ USE hotel_management;
 -- xóa trigger nếu đã tồn tại
 
 -- viet code tạo trigger ở đây
+
+-- chức năng 8
+-- tạo trigger để tự động gửi thông báo khi đặt phòng
+CREATE TRIGGER TRG_NotifyBooking
+ON Bookings
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO Notifications (Notification_ID, User_ID, Message)
+    SELECT 
+        CONCAT('N', I.Booking_ID),
+        I.User_ID,
+        N'Đặt phòng thành công!'
+    FROM INSERTED I;
+END
