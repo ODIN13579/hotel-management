cfeef5aa0105708bed7ab3f2b29ec1d37356b1ca
nhuhuynh17 <nhuhuynh123gd@gmail.com>
2026-03-30 22:42:55 +0700

    Create trigger for invoice total calculation
    Add trigger to calculate total amount for invoices before insertion.

--- database/create_trigger.sql
+++ database/create_trigger.sql
@@ -4,6 +4,16 @@ USE hotel_management;
 
 -- viet code tạo trigger ở đây
 
+-- Trigger chức năng 5: Tính tiền invoice
+DELIMITER //
+CREATE TRIGGER trg_invoice_total
+BEFORE INSERT ON Invoices
+FOR EACH ROW
+BEGIN
+    SET NEW.Total_Amount = calculate_total(NEW.Booking_ID);
+END //
+DELIMITER ;
+
 -- chức năng 8
 -- tạo trigger để tự động gửi thông báo khi đặt phòng
 CREATE TRIGGER TRG_NotifyBooking
