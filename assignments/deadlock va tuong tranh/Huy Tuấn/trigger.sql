d29827ac2a439b9bbab9b7ef9e870044737a3919
HuyTuan <tuanhuy2342006@gmail.com>
2026-04-06 23:34:27 +0700

    update Huy Tuấn

---
+++ assignments/deadlock va tuong tranh/Huy Tuấn/trigger.sql
@@ -0,0 +1,60 @@
+-- chức năng 8
+-- tạo trigger để tự động gửi thông báo khi đặt phòng
+CREATE TRIGGER TRG_NotifyBooking
+ON Bookings
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO Notifications (Notification_ID, User_ID, Message)
+    SELECT 
+        CONCAT('N', I.Booking_ID),
+        I.User_ID,
+        N'Đặt phòng thành công!'
+    FROM INSERTED I;
+END
+
+
+-- chức năng liên kết số 1
+-- tạo trigger để tự động cập nhật trạng thái phòng khi đã xác nhận booking
+CREATE TRIGGER TRG_UpdateRoomStatus
+ON Bookings
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    UPDATE Rooms R
+    SET R.Status = N'Booked'
+    FROM Rooms R
+    -- JOIN với bảng tạm Bookings để lấy thông tin về phòng đã được đặt
+    JOIN INSERTED I ON R.Room_ID = I.Room_ID
+    WHERE I.Status = N'Confirmed';
+END
+
+
+--tạo trigger để tự động cập nhật trạng thái payment ( đang chờ xử lý) khi đã xác nhận booking
+CREATE TRIGGER TRG_UpdatePaymentStatus
+ON Bookings
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    UPDATE Payment P 
+    SET P.Status = N'Pending'
+    FROM Payment P
+    -- JOIN với bảng tạm Bookings để lấy thông tin về booking đã được xác nhận
+    JOIN INSERTED I ON P.Booking_ID = I.Booking_ID
+    WHERE I.Status = N'Confirmed';
+END
+
+
+--tạo trigger để tự động cập nhật trạng thái employee (processing ) khi đã xác nhận booking
+CREATE TRIGGER TRG_UpdateEmployeeStatus
+ON Bookings
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    UPDATE Employees E 
+    SET E.Status = N'Processing'
+    FROM Employees E
+    -- JOIN với bảng tạm Bookings để lấy thông tin về booking đã được xác nhận
+    JOIN INSERTED I ON E.Employee_ID = I.Employee_ID
+    WHERE I.Status = N'Confirmed';
+END
