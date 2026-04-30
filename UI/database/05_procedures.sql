b4dd3d3d6ddd79c24c253a9bc89c35c6a3dc8761
faconmao <anhgiau115@gmail.com>
2026-04-30 22:50:38 +0700

    update pc

--- UI/database/05_procedures.sql
+++ UI/database/05_procedures.sql
@@ -202,53 +202,43 @@ BEGIN
 END
 GO
 
--- 9. "Xác nhận" (Booking: Đã xác nhận | Room: đã đặt)
+-- 9. "Xác nhận" (Chỉ cập nhật Bookings, bảng Rooms để Trigger tự lo)
 CREATE OR ALTER PROCEDURE sp_ConfirmBooking
     @BookingID VARCHAR(50)
 AS
 BEGIN
     SET NOCOUNT ON;
     UPDATE Bookings SET Status = N'Đã xác nhận' WHERE Booking_ID = @BookingID;
-    
-    UPDATE Rooms SET Status = N'đã đặt'
-    WHERE Room_ID = (SELECT Room_ID FROM Bookings WHERE Booking_ID = @BookingID);
 END;
 GO
 
--- 10. "Nhận phòng" (Booking: Đã nhận phòng | Room: đã nhận)
+-- 10. "Nhận phòng" (Chỉ cập nhật Bookings, bảng Rooms để Trigger tự lo)
 CREATE OR ALTER PROCEDURE sp_CheckInBooking
     @BookingID VARCHAR(50)
 AS
 BEGIN
     SET NOCOUNT ON;
     UPDATE Bookings SET Status = N'Đã nhận phòng' WHERE Booking_ID = @BookingID;
-    
-    UPDATE Rooms SET Status = N'đã nhận'
-    WHERE Room_ID = (SELECT Room_ID FROM Bookings WHERE Booking_ID = @BookingID);
 END;
 GO
--- 11. "Trả phòng" (Booking: Đã trả phòng | Room: có sẵn)
+
+-- 11. "Trả phòng" (Chỉ cập nhật Bookings, bảng Rooms để Trigger tự lo)
 CREATE OR ALTER PROCEDURE sp_CheckOutBooking
     @BookingID VARCHAR(50)
 AS
 BEGIN
     SET NOCOUNT ON;
     UPDATE Bookings SET Status = N'Đã trả phòng' WHERE Booking_ID = @BookingID;
-    
-    UPDATE Rooms SET Status = N'có sẵn'
-    WHERE Room_ID = (SELECT Room_ID FROM Bookings WHERE Booking_ID = @BookingID);
 END;
 GO
--- 12."Hủy" (Booking: Đã hủy | Room: có sẵn)
+
+-- 12."Hủy" (Chỉ cập nhật Bookings, bảng Rooms để Trigger tự lo)
 CREATE OR ALTER PROCEDURE sp_CancelBooking
     @BookingID VARCHAR(50)
 AS
 BEGIN
     SET NOCOUNT ON;
     UPDATE Bookings SET Status = N'Đã hủy' WHERE Booking_ID = @BookingID;
-    
-    UPDATE Rooms SET Status = N'có sẵn'
-    WHERE Room_ID = (SELECT Room_ID FROM Bookings WHERE Booking_ID = @BookingID);
 END;
 GO
 
